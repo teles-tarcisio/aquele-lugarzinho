6a1c112b339c57da2e9341d12ab9839854e21d88
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-08-11 15:03:48 -0300

    frontend img upload with imgbb api (WIP)

--- frontend/src/components/AddContentButtons/AddContentButtons.jsx
+++ frontend/src/components/AddContentButtons/AddContentButtons.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+/* eslint-disable no-restricted-syntax */
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
 import {
   Button,
   Dialog,
@@ -6,13 +8,18 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Input,
+  InputLabel,
   Stack,
   TextField,
 } from '@mui/material';
 import * as api from '../../services/api';
+import CustomUploadButton from './CustomUploadButton';
 
 export default function AddContentButtons({ token }) {
   const [openDialog, setOpenDialog] = useState(false);
+  const filesElement = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   function handleAddLocation(ev) {
     console.log(ev.target);
@@ -20,6 +27,19 @@ export default function AddContentButtons({ token }) {
   }
   function handleClose() {
     setOpenDialog(false);
+  }
+
+  function handleFileChange(ev) {
+    console.log(ev.target.files);
+    setSelectedFile(ev.target.files[0]);
+  }
+
+  async function handleSend() {
+    const body = new FormData();
+    body.set('key', '34659037bd18afd2a6b33f03fbe71b5e');
+    body.append('image', selectedFile);
+    const result = await axios.post('https://api.imgbb.com/1/upload', body);
+    console.log(result.data);
   }
 
   return (
@@ -56,7 +76,6 @@ export default function AddContentButtons({ token }) {
             variant="standard"
           />
           <TextField
-            autoFocus
             margin="dense"
             id="name"
             label="Endereço do lugar"
@@ -64,10 +83,22 @@ export default function AddContentButtons({ token }) {
             fullWidth
             variant="standard"
           />
+          {/* <CustomUploadButton /> */}
+          <Input
+            margin="dense"
+            id="image"
+            label="Anexe sua imagem"
+            type="file"
+            ref={filesElement}
+            accept="image/*"
+            fullWidth
+            variant="standard"
+            onChange={handleFileChange}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose}>Enviar</Button>
+          <Button onClick={handleSend}>Enviar</Button>
         </DialogActions>
       </Dialog>
     </>
